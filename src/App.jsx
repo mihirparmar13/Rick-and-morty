a90a5b167eedf1dd32cb792b7ce9d56811393e10
Mihir7878 <m.b.parmar5650@gmail.com>
2024-12-21 19:20:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Support from './Support/Support';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/support">
+          <Support />
+        </Route>
+      </Switch>                                                                                                                                                                                                                                                                                                   
+    </Router>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/Support/Support.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import './Support.css'
+import Head from '../header/Head'
+import Foot from '../Footer/Foot'
+const Support = () => {
+  return (
+    <div>
+      <Head/>
+      <Foot/>
+    </div>
+  )
+}
+
+export default Support
